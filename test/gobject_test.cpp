76e50bf3309ea62c1117ff397f9c90939120c1f6
Walter Fan <walter.fan@gmail.com>
2024-03-17 22:48:36 +0800

    update example

--- test/gobject_test.cpp
+++ test/gobject_test.cpp
@@ -52,4 +52,27 @@ TEST_F(GObjectTest, set_get_property)
     g_object_unref (instance2);
   
 }
+//also can check the number of instance of a type with g_type_instance_count
+//note:  export GOBJECT_DEBUG=instance-count
+TEST_F(GObjectTest, check_ref_count)
+{
+    GObject * obj1 = (GObject *)g_object_new (G_TYPE_OBJECT, NULL);
+
+    g_assert_nonnull (obj1);
+
+    // the contents of the pointer are reset to NULL when the last reference to
+    // the GObject instance goes away; by passing a pointer to the same instance
+    // we can check it for NULL later
+    g_object_add_weak_pointer (G_OBJECT (obj1), (gpointer *) &obj1);
+
+    // ...test FooObject...
+
+    // drop the last reference
+    g_object_unref (obj1);
+
+    // at this point, the object should be NULL if nothing is holding
+    // an additional reference.
+    g_assert_null (obj1);
+
+}
 
---
+++ test/gst_util_test.cpp
@@ -0,0 +1,19 @@
+#include "gtest/gtest.h"
+#include "string_util.h"
+#include "time_util.h"
+#include "time.h"
+#include <stdint.h>
+#include <string>
+#include <iostream>
+#include <memory>
+#include <vector>
+
+using namespace std;
+using namespace testing;
+
+
+TEST(GstUtilTest, probe)
+{
+
+
+}
